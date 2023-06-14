95af5d18ef2c2f6bb5655f3b34bffec38cfc2e56
anam2 <andynam1997@gmail.com>
2023-06-13 21:42:04 -0700

    Added one more test case

--- SimpsonsTests/SimpsonsTests.swift
+++ SimpsonsTests/SimpsonsTests.swift
@@ -8,12 +8,23 @@
 import XCTest
 
 final class SimpsonsTests: XCTestCase {
+    let character1 = CharacterListData(name: "Character1",
+                                       fullDescription: "Character1 - This is a sample description",
+                                       iconImage: Icon(url: "", height: "", width: ""))
+
+    override func setUp() {
+        super.setUp()
+    }
 
     func testSimpsonURLIsNotNil() {
         let url = URL(string: "http://api.duckduckgo.com/?q=simpsons+characters&format=json")!
         URLSession.shared.dataTask(with: url) { (data, response, error) in
             XCTAssertNotNil(data, "Data should not be nil.")
         }
+    }
+
+    func testDescriptionTextToMakeSureCharacterNameIsNotIncluded() {
+        XCTAssertEqual(character1.descriptionText, "This is a sample description")
     }
 
 }
